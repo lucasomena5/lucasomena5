3f0b4cddd2eed2f67ef156e779fa6dd8f4cae061
Lucas Omena <lucasomena5@gmail.com>
2025-07-30 11:28:00 -0700

    feat: included Jenkinsfile for terraform

--- devsecops/jenkins/pipelines/terraform/aws-s3/s3-bucket.tf
+++ devsecops/jenkins/pipelines/terraform/aws-s3/s3-bucket.tf
@@ -1,8 +1,4 @@
+data "aws_account_primary_contact" "cloud_user" {}
 resource "aws_s3_bucket" "my_bucket" {
-  bucket = "my-unique-bucket-name-905418039818"
-
-  tags = {
-    Name        = "MyBucket"
-    Environment = "Dev"
-  }
+  bucket = "my-unique-bucket-name-${data.aws_account_primary_contact.cloud_user.account_id}"
 }
--- devsecops/jenkins/pipelines/terraform/aws-s3/variables.tf
+++ devsecops/jenkins/pipelines/terraform/aws-s3/variables.tf
@@ -2,11 +2,11 @@
 variable "region" {
   type        = string
   description = "(Required) AWS Region"
-  default     = "us-west-1"
-  validation {
-    condition     = contains(["us-west-1"], var.region)
-    error_message = "The current support value is us-west-1."
-  }
+  default     = "us-east-1"
+  # validation {
+  #   condition     = contains(["us-west-1"], var.region)
+  #   error_message = "The current support value is us-west-1."
+  # }
 }
 
 variable "access_key" {
